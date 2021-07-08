fa71fb223eb95b0f872a52eb03ca938c3cf6eb31
Max McKenzie <max@seam.es>
2021-07-08 10:47:19 +0000

    Release 0.5.7

--- Formula/snippr.rb
+++ Formula/snippr.rb
@@ -5,20 +5,20 @@
 class Snippr < Formula
   desc ""
   homepage ""
-  version "0.5.6"
+  version "0.5.7"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/snippr-io/homebrew-cli/releases/download/0.5.6/snippr-darwin-amd64.tar.gz"
-      sha256 "16b977b7682f40fbcbff5628716af7093e1aebb0e43b62161c709fa5e04f8806"
+      url "https://github.com/snippr-io/homebrew-cli/releases/download/0.5.7/snippr-darwin-amd64.tar.gz"
+      sha256 "df187d616b0b0033402ba177457d4fb197a6bc027d8d894a3e65c9378b1a35f7"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/snippr-io/homebrew-cli/releases/download/0.5.6/snippr-linux-amd64.tar.gz"
-      sha256 "6dec7c83463841f8f766bc0ac6901bbcee07c05502fec7b06a377a96311d8f83"
+      url "https://github.com/snippr-io/homebrew-cli/releases/download/0.5.7/snippr-linux-amd64.tar.gz"
+      sha256 "26fa3fcdfc2e394d15ccc4765bc2a3564a0bbbeedae96e4a7f02327f6f78ab50"
     end
   end
 
